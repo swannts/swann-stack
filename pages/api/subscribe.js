649456164eee57ebe394aa755d1655f3bd23f6e6
swann <swann@fortunatis.ch>
2023-09-28 00:01:22 +0600

    Removed subscribe

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -13,28 +13,28 @@ const subscribe = async (req, res) => {
   if (!email) {
     return res.status(400).json({ error: 'Email is required.' })
   }
-
-  try {
-    const FORM_ID = process.env.CONVERTKIT_FORM_ID
-    const API_KEY = process.env.CONVERTKIT_API_KEY
-    const API_URL = process.env.CONVERTKIT_API_URL
-
-    const data = { email, api_key: API_KEY }
-
-    const response = await fetch(`${API_URL}forms/${FORM_ID}/subscribe`, {
-      body: JSON.stringify(data),
-      headers: { 'Content-Type': 'application/json' },
-      method: 'POST',
-    })
-
-    if (response.status >= 400) {
-      return res.status(400).json({ error: 'There was an error subscribing to the list.' })
-    }
-
-    return res.status(201).json({ error: '' })
-  } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() })
-  }
+  return '';
+  // try {
+  //   const FORM_ID = process.env.CONVERTKIT_FORM_ID
+  //   const API_KEY = process.env.CONVERTKIT_API_KEY
+  //   const API_URL = process.env.CONVERTKIT_API_URL
+  //
+  //   const data = { email, api_key: API_KEY }
+  //
+  //   const response = await fetch(`${API_URL}forms/${FORM_ID}/subscribe`, {
+  //     body: JSON.stringify(data),
+  //     headers: { 'Content-Type': 'application/json' },
+  //     method: 'POST',
+  //   })
+  //
+  //   if (response.status >= 400) {
+  //     return res.status(400).json({ error: 'There was an error subscribing to the list.' })
+  //   }
+  //
+  //   return res.status(201).json({ error: '' })
+  // } catch (error) {
+  //   return res.status(500).json({ error: error.message || error.toString() })
+  // }
 }
 
 export default subscribe
